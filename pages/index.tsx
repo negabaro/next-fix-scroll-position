201fa5ad106b98f67c4efd312ffcb30739c45874
negabaro <negabaro@naver.com>
2022-03-03 22:50:16 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋</h1>
+    <Link href="/about">
+      <h1>Hello Next.js 👋</h1>
+    </Link>
+
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
